c0d597d3cab3a0d1410f1324e5114d79c106abc6
archita2006 <65909984+archita2006@users.noreply.github.com>
2021-07-21 14:44:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { createAppContainer,createSwitchNavigator } from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs'
+import{ Stylesheet, Text, View } from 'react-native';
+import WriteStoryScreen from './screens//WriteStoryScreen';
+import ReadStoryScreen from './screens//ReadStoryScreen';
+
+
+
+export default class App extends React.Component{
+  render(){
+   return (
+<View>
+  
+</View>
+  
+   );
+}
+} 
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+``  
